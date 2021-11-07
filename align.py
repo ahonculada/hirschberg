1d99a0f703d54e5335988fbe57ac610ee04d5083
aaron <aaron@pop-os.localdomain>
2021-11-07 09:22:09 -0500

    global alignment in linear space, still need reconstruct alignment

--- align.py
+++ align.py
@@ -13,11 +13,11 @@ class Align:
         self.path = []
         self.alignment_1 = ''
         self.alignment_2 = ''
-        self.forward = [[0 for _ in range(len(B)+1)] for _ in range(2)]
-        self.reverse = [[0 for _ in range(len(B)+1)] for _ in range(2)]
+        self.forward = [[Cell(0) for _ in range(len(B)+1)] for _ in range(2)]
+        self.reverse = [[Cell(0) for _ in range(len(B)+1)] for _ in range(2)]
         self.min_alignment_score = -1
-        self.naive_align(A, B)
-        #self.hirsch_align(A, B)
+        #self.naive_align(A, B)
+        self.hirsch_align(A, B)
 
     def reconstruct_path(self, row: int, col: int):
         if not row and not col:
@@ -70,29 +70,29 @@ class Align:
     def _hirsch_align(self, A1: int, A2: int, B1: int, B2: int):
         # no more characters in A
         if A2 <= A1:
-            for i in range(B1, B2):
+            for j in range(B1, B2):
                 self.alignment_1 += '-'
-                self.alignment_2 += self.B[i]
+                self.alignment_2 += self.B[j]
             return
         # no more characters in B
         if B2 <= B1:
-            for i in range(B1, B2):
-                self.alignment_1 += self.B[i]
+            for j in range(B1, B2):
+                self.alignment_1 += self.B[j]
                 self.alignment_2 += '-'
             return
         # A has exactly one character
         if A1+1 == A2:
             c = self.A[A1]
             memo = B1
-            for i in range(B1+1, B2):
-                if self.B[i] == c:
-                    memo = i
-            for i in range(B1, B2):
-                if i == memo:
+            for j in range(B1+1, B2):
+                if self.B[j] == c:
+                    memo = j
+            for j in range(B1, B2):
+                if j == memo:
                     self.alignment_1 += c
                 else:
                     self.alignment_1 += '-'
-                self.alignment_2 += self.B[i]
+                self.alignment_2 += self.B[j]
             return
 
         # Divide and Conquer
@@ -102,43 +102,52 @@ class Align:
         k_star = B1
         best = float('inf')
         # find cheapest split
-        for i in range(B1, B2):
-            S = self.forward[mid%2][i] + self.reverse[mid%2][i]
+        for j in range(B1, B2+1):
+            S = self.forward[mid%2][j].val + self.reverse[mid%2][j].val
             if S < best:
-                best, k_star = S, i
+                best, k_star = S, j
         if self.min_alignment_score == -1:
             self.min_alignment_score = best
 
-        self._hirsch_align(A1, mid, B1, k_star)
-        self._hirsch_align(mid, A2, k_star, B2)
+        # recursive calls to reconstruct alignment
+        #self._hirsch_align(A1, mid, B1, k_star)
+        #self._hirsch_align(mid, A2, k_star, B2)
 
     def set_forward(self, A1: int, A2: int, B1: int, B2: int):
-        self.forward[A1%2][B1] = 0
+        self.forward[A1%2][B1].val = 0
         # set first row
         for j in range(B1+1, B2+1):
-            self.forward[A1%2][j] = self.forward[A1%2][j-1] + 1
+            self.forward[A1%2][j].val = self.forward[A1%2][j-1].val + 1
+        #print(self.forward[A1%2][B1:B2+1].val)
+
         for i in range(A1+1, A2+1):
-            self.forward[i%2][B1] = self.forward[(i-1)%2][B1] + 1
+            # set first column
+            self.forward[i%2][B1].val = self.forward[(i-1)%2][B1].val + 1
             for j in range(B1+1, B2+1):
-                self.forward[i%2][j] = min(
-                    self.forward[(i-1)%2][j] + 1,
-                    self.forward[i%2][j-1] + 1,
-                    self.forward[(i-1)%2][j-1] + 1 * (self.A[i-1] == self.B[j-1])
+                self.forward[i%2][j].val = min(
+                    self.forward[(i-1)%2][j].val + 1,
+                    self.forward[i%2][j-1].val + 1,
+                    self.forward[(i-1)%2][j-1].val + 1 * (self.A[i-1] != self.B[j-1])
                 )
+            #print(self.forward[i%2][B1:B2+1].val)
 
     def set_reverse(self, A1: int, A2: int, B1: int, B2: int):
-        self.reverse[A2%2][B2] = 0
+        self.reverse[A2%2][B2].val = 0
         # set first row
         for j in range(B2-1, B1-1, -1):
-            self.reverse[A2%2][j] = self.reverse[A2%2][j+1] + 1
+            self.reverse[A2%2][j].val = self.reverse[A2%2][j+1].val + 1
+        #print(self.reverse[A1%2][B1:B2+1].val)
+
         for i in range(A2-1, A1-1, -1):
-            self.reverse[i%2][B2] = self.reverse[(i+1)%2][B2] + 1
+            # set first column
+            self.reverse[i%2][B2].val = self.reverse[(i+1)%2][B2].val + 1
             for j in range(B2-1, B1-1, -1):
-                self.reverse[i%2][j] = min(
-                    self.reverse[(i+1)%2][j] + 1,
-                    self.reverse[i%2][j+1] + 1,
-                    self.reverse[(i+1)%2][j+1] + 1 * (self.A[i-1] == self.B[j-1])
+                self.reverse[i%2][j].val = min(
+                    self.reverse[(i+1)%2][j].val + 1,
+                    self.reverse[i%2][j+1].val + 1,
+                    self.reverse[(i+1)%2][j+1].val + 1 * (self.A[i] != self.B[j])
                 )
+            #print(self.reverse[i%2][B1:B2+1].val)
 
     def print_DP(self):
         for R in self.DP:
@@ -151,11 +160,11 @@ class Align:
         print("Second String: {}".format(self.B))
         #self.print_DP()
         print("Minimum Alignment Score: {}".format(self.min_alignment_score))
-        print()
-        self.reconstruct_path(len(self.A), len(self.B))
-        print("Alignment:")
-        print(self.alignment_1)
-        print(self.alignment_2)
+        #print()
+        #self.reconstruct_path(len(self.A), len(self.B))
+        #print("Alignment:")
+        #print(self.alignment_1)
+        #print(self.alignment_2)
 
 
 def read_in() -> (str, str):
